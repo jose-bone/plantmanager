f625f993469b0a10a1206ef3a907457e503743b7
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-10-15 18:58:36 +0200

    Stack routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Welcome } from "../pages/Welcome";
+import { UserIdentification } from "../pages/UserIdentification";
+import { Confirmation } from "../pages/Confirmation";
+import { PlantSave } from "../pages/PlantSave";
+import { MyPlants } from "../pages/MyPlants";
+import AuthRoutes from "./tab.routes";
+
+import colors from "../styles/colors";
+
+const stackRoutes = createNativeStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <stackRoutes.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: colors.white },
+      headerShown: false,
+    }}
+  >
+    <stackRoutes.Screen name="Welcome" component={Welcome} />
+
+    <stackRoutes.Screen
+      name="UserIdentification"
+      component={UserIdentification}
+    />
+
+    <stackRoutes.Screen name="Confirmation" component={Confirmation} />
+
+    <stackRoutes.Screen name="PlantSelect" component={AuthRoutes} />
+
+    <stackRoutes.Screen name="PlantSave" component={PlantSave} />
+
+    <stackRoutes.Screen name="MyPlants" component={AuthRoutes} />
+  </stackRoutes.Navigator>
+);
+
+export default AppRoutes;
